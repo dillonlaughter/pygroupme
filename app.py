5e679ce67794dfc12d776fcd8843c3ce49645e05
dillonlaughter <39819735+dillonlaughter@users.noreply.github.com>
2019-09-07 23:51:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# RESOURCE: http://www.apnorton.com/blog/2017/02/28/How-I-wrote-a-Groupme-Chatbot-in-24-hours//
+# RESOURCE: http://www.apnorton.com/blog/2017/02/28/How-I-wrote-a-Groupme-Chatbot-in-24-hours/
 
 
 # IMPORTS
