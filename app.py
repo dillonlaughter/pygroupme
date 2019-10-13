aa57d8dff2a4173baa6403c662b9585badc7609b
dillonlaughter <39819735+dillonlaughter@users.noreply.github.com>
2019-10-12 19:10:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -371,7 +371,7 @@ def dundermain():
 ##        print(arr_ppl)
         send_data(arr_ppl)
     print('time: ',time.time()-a)
-    replt('/run')
+    reply('/run')
 
 def next_person(data):
     ### sorts arr_ppl to get the smallest date, returns the arr_ppl[index]
